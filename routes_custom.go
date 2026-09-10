247d2f4a3c0750e64cfe9f61fcebeada6a8295a7
Katriel <katriel.developer@gmail.com>
2026-09-10 09:18:03 -0300

    chore: disable form endpoint until supported

--- routes_custom.go
+++ routes_custom.go
@@ -5,6 +5,8 @@ import "github.com/justinas/alice"
 
 // registerCustomRoutes houses your production payment features
 func (s *server) registerCustomRoutes(c alice.Chain) {
+	s.router.Handle("/chat/send/cta", c.Then(s.SendCTA())).Methods("POST")
+	// s.router.Handle("/chat/send/form", c.Then(s.SendForm())).Methods("POST")
 	s.router.Handle("/chat/send/pix", c.Then(s.SendPix())).Methods("POST")
 
 	// Any future endpoints you build (like /chat/send/charge) can go here:
